fd436d9e9d4349be78b3a9f71ce0d45f27ee0d10
Maiconfz <maiconfz@gmail.com>
2025-02-23 05:16:53 +0000

    Spring init

---
+++ src/main/java/io/maiconfz/spring_kafka_rabbitmq_ping_pong/SpringKafkaRabbitmqPingPongApplication.java
@@ -0,0 +1,13 @@
+package io.maiconfz.spring_kafka_rabbitmq_ping_pong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaRabbitmqPingPongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaRabbitmqPingPongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/maiconfz/spring_kafka_rabbitmq_ping_pong/SpringKafkaRabbitmqPingPongApplicationTests.java
@@ -0,0 +1,13 @@
+package io.maiconfz.spring_kafka_rabbitmq_ping_pong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaRabbitmqPingPongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
